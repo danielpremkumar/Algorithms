a35f106aef680734a7a34c5d00512e1fb737d469
nerdylegend <31123377+nerdylegend@users.noreply.github.com>
2019-08-31 15:59:08 +0530

    Recursion update

--- src/com/company/Recursion.java
+++ src/com/company/Recursion.java
@@ -1,10 +1,12 @@
 package com.company;
 
 import java.math.BigInteger;
+import java.util.logging.Logger;
 
 public class Recursion {
+    static Logger logger  = Logger.getLogger(Recursion.class.getName());
     public static void main(String[] args) {
-        System.out.println(factorial(5000));
+        logger.info(factorial(5000).toString());
     }
 
     private static BigInteger factorial(int num) {
@@ -15,7 +17,9 @@ public class Recursion {
      * Tail Recursion (Uses the same Stack frame in every recursion,
      * so stackoverflow does not occur)
      * Last statement in the method directly returns value, so maintaining
-     * state of previous stack in not needed
+     * state of previous stack in not needed.
+     * Intermediate value stored and passed to recursive
+     * function using variables like accumulator.
      */
     private static BigInteger factHelper(int num, BigInteger accumulator) {
         if (num <= 1) return accumulator;
